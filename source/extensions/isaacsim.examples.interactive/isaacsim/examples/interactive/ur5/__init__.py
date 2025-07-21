1f49104dea26395fbcc51750673db4abdd8dfe57
inaki1111 <inakirm111@gmail.com>
2025-07-21 14:10:37 +0200

    add sim2real title

--- source/extensions/isaacsim.examples.interactive/isaacsim/examples/interactive/ur5/__init__.py
+++
@@ -1,4 +0,0 @@
-# NOTE: Import here your extension examples to be propagated to ISAAC SIM Extensions startup
-
-from isaacsim.examples.interactive.ur5.ur5_example import UR5GraspExample
-from isaacsim.examples.interactive.ur5.ur5_extension import UR5ExampleExtension
